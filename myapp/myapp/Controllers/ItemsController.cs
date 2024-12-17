da1585b6180ea1f2eaa7bf0093d3567632c7b284
saifahmed34 <saifdragonball5@gmail.com>
2024-12-17 13:22:16 +0200

    first asp.net MVC project

---
+++ myapp/myapp/Controllers/ItemsController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using myapp.Data;
+using myapp.Models;
+namespace myapp.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly MyAppContext _context;
+        public ItemsController(MyAppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var item = await _context.Items.ToListAsync();
+            return View(item);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id, Name, Price")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Items.Add(item);
+                await _context.SaveChangesAsync();
+               return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+        public async Task<IActionResult>Edit(int id)
+        {
+            var item =await _context.Items.FirstOrDefaultAsync(x => x.Id == id);
+            return View(item);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Name, Price")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var item = await _context.Items.FirstOrDefaultAsync(x => x.Id == id);
+            return View(item);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var item = await _context.Items.FindAsync(id);
+            if (item != null)
+            {
+                _context.Items.Remove(item);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
+
---
+++ myapp/myapp/Data/MyAppContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using myapp.Models;
+
+namespace myapp.Data
+{
+    public class MyAppContext : DbContext
+    {
+        public MyAppContext(DbContextOptions<MyAppContext> options) { }
+
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Use your actual connection string here
+                optionsBuilder.UseSqlServer("Data Source=SAIF-AHMED\\SQL2022;Initial Catalog=myapp_database;Integrated Security=True;Pooling=False;Encrypt=False;Trust Server Certificate=True");
+            }
+        }
+    }
+}
+
+
